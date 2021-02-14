609283e4c037956ce9cd7d8ff92367bf0b5e49c9
Omar <omarmuhammed1998.om@gmail.com>
2021-02-14 23:43:15 +0200

    rendering templates in the views

--- blogpost/blog/views.py
+++ blogpost/blog/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {
+        'author': 'OmarMA',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'February 14, 2021'
+    },
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'February 15, 2021'
+    },
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {
+        'title': 'About'
+    })
