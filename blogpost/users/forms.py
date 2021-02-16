cbb3f951e2f4686b253e07c80471cc66905f9776
Omar <omarmuhammed1998.om@gmail.com>
2021-02-16 13:20:12 +0200

    Added first name and last name to as required fields

--- blogpost/users/forms.py
+++ blogpost/users/forms.py
@@ -5,11 +5,15 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
 
     class Meta:
         model = User
         fields = [
             'username',
+            'first_name',
+            'last_name',
             'email',
             'password1',
             'password2'
